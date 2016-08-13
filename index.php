c32fb2335a773ace2a2b6cfd91e936619c618001
root <root@localhost.localdomain>
2016-08-13 08:15:33 +0200

    clear dir

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $load = @$_GET['disp'];
-  if($load == 'gui'){
-    require_once('guiIndex.php');
-  } else {
-    require_once('termIndex.php');
-  }
-?>
--- projects.php
+++
@@ -1,4 +0,0 @@
-<h1 style="font-size:100px;text-align:center;">
-  projekte <br>
-  blabla
-<h1>
